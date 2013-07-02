af6884d6ea8417b79cfaf357e424483cea2ee109
robsbq <robsbq@gmail.com>
2013-07-02 00:39:54 +0000

    add docs projeto

---
+++ 03 - Analise/Teste/Arquivos SIKULI/revendedor_teste.sikuli/teste.py
@@ -0,0 +1,78 @@
+click("IxiZ.png")
+click("DadosPrincip.png")
+
+paste("REVENDEDORA.png", "MARIA PAULA DE RESENDE")
+paste("ENDERECO.png", "RUA 7 SETEMBRO")
+paste("1343181360279.png", "123")
+paste("ZOMPLEMENTO.png", "55A")
+paste("CIDADE.png", "VARGINHA")
+type("UFIselec.png", "MG")
+paste("CEP.png", "36340000")
+paste("CPF-2.png", "06342045600")
+paste("RG.png", "MG13177794")
+paste("ATADENASCIME.png", "05101985")
+type("CUDIGOCONFIS.png", "123")
+paste("NOTAFISCALSI.png", "123")
+type("DECREDITO.png", "2000")
+paste("1343183227544.png", "testes")
+
+click("1343183546863.png")
+paste("TELEFONERESI.png", "3233541368")
+paste("CELULAR1.png", "3233541234")
+paste("TELEFONECOME.png", "2123451234")
+paste("CELULAR2.png", "3212341222")
+paste("EMAIL1.png", "123@34345")
+paste("EMAIL2.png", "123@34556")
+click("INFORMAQOESC-1.png")
+paste("CONJUGUE-1.png", "MARIA APARECIDA")
+
+paste("ENDERECO-1.png", "RUA 7 SETEMBRO")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
