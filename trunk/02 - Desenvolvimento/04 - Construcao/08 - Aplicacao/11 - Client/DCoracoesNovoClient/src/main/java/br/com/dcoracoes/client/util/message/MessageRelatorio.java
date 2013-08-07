6ecc089629b7c9d90edaee615fd10f02ef8e45b8
robsbq@gmail.com <robsbq@gmail.com@56808911-a594-bd40-216b-f16e91c3f8d2>
2013-08-07 01:00:16 +0000

    add logica para FormVenda.

---
+++ trunk/02 - Desenvolvimento/04 - Construcao/08 - Aplicacao/11 - Client/DCoracoesNovoClient/src/main/java/br/com/dcoracoes/client/util/message/MessageRelatorio.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.dcoracoes.client.util.message;
+
+/**
+ *
+ * @author Jose Robson
+ */
+public class MessageRelatorio {
+    
+    public static final String MENSAGEM_ERRO_RELATORIO_EXPEDICAO_VENDA = "Erro ao gerar relatório expedição de venda.";
+    public static final String MENSAGEM_ERRO_RELATORIO_COMPLETO_VENDA = "Erro ao gerar relatório completo de venda.";
+    public static final String MENSAGEM_ERRO_RELATORIO_COMPLETO_COMPRA = "Erro ao gerar relatório completo de compra.";
+    public static final String MENSAGEM_ERRO_RELATORIO_EXPEDICAO_COMPRA = "Erro ao gerar relatório expedição de compra.";
+}
